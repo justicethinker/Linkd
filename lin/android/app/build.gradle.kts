a938329ee595ea8a923281894c718027cf577b37
justicethinker2 <justicethinker0@gmail.com>
2026-02-22 05:15:24 +0000

    build update

--- lin/android/app/build.gradle.kts
+++ lin/android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.linkd.linkd_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion "25.2.9519653"
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
